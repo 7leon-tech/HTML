e494b474096109d32705677005c6d621378ce81e
Ilkom1a <68373160+Ilkom1a@users.noreply.github.com>
2020-10-21 13:19:02 +0700

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EL Account</title>
+		<style type="text/css">
+			<!--header{ padding:65px; font-size:3em;}
+			.first{paddding:40px;}
+			.main1{padding:10px;}
+			.main2 a:hover{color:black; font-size:2em;}
+			.daden{padding:2px;}
+			input[type=text]{border: 1px solid #EBC481; border-radius: 8px; padding:9px; width: 150px;}
+			select,input[type=email],textarea {border: 1px solid #EBC481; border-radius: 8px; padding:9px; width: 225px;}
+			input[type=submit] {border-radius: 5px; border: 1px solid #EBC481; margin: 4px 10px; padding: 5px; width: 100px;}
+			input[type=email]:focus,input[type=email]:hover{width:250px; padding:13px; border-color:#EBC481; border-radius: 9px; box-shadow: inset 0 1px 1px #EBC481, 0 0 8px #EBC481; outline: 0;}
+			input[type=text]:focus,input[type=text]:hover{width:180px; padding:13px; border-color:#EBC481; border-radius: 9px; box-shadow: inset 0 1px 1px #EBC481, 0 0 8px #EBC481; outline: 0;}
+			textarea:focus,textarea:hover{width:250px; padding:13px; border-color:#EBC481; border-radius: 9px; box-shadow: inset 0 1px 1px #EBC481, 0 0 8px #EBC481; outline: 0;}
+			input[type=password]:focus,input[type=password]:hover{width:250px; padding:13px; border-color:#EBC481; border-radius: 9px; box-shadow: inset 0 1px 1px #EBC481, 0 0 8px #EBC481; outline: 0;}
+			input[type=submit]:focus,input[type=submit]:hover{border-color:#EBC481; border-radius: 9px; box-shadow: inset 0 1px 1px #EBC481, 0 0 8px #EBC481; outline: 0;}-->
+		</style>
+</head>
+<body>
+	<header><center><img src="cats-crop.jpg" width="60%" height="60%"></center></header>
+	<div class="first">
+	<table width="120%">
+	<td width="30%"></td>
+	<td width="60%">
+	<a style="font-family:MoolBoran;font-size:1.5em;">Please fill these form to make your EL account</a>
+	<form method="post">
+				<div class="daden"><input type="text" name="nama" style="align:center;" placeholder="FirstName..."> 
+				<input type="text" name="nama" style="align:center;" placeholder="LastName...">
+				</div>
+				<div class="daden"><label>Gender:</label><input type="radio" name="gender" value="male">Male<input type="radio" name="gender" value="Female">Female
+				</div>
+				<div class="daden"><textarea name="alamat" cols="25" rows="2" placeholder="Enter your Address.."></textarea>
+				</div>
+				<div class="daden"><input type="text" name="hp" style="width: 225px;" placeholder="Your PhoneNumber...">
+				</div>
+				<div class="daden"><input type="email" name="surel" placeholder="Enter your Email...">
+				</div></div>
+	<td width="30%"></td>
+	</table>
+	<br>
+		<br>						
+		<br>
+		<br>
+		<br>
+	</div>
+	<footer style="font-family:MoolBoran;font-size:1em;"><center><b>Copyright KasandiCorps@2017</b></center></footer>
+</body>
+</html>
